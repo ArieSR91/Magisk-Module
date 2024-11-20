b1822f628023070fd29f8b8331c32b408bf5fddf
ArieSR91 <syahri.ramadhan2022@gmail.com>
2024-11-20 18:44:36 +0800

    from debian

--- download.sh
+++ download.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 CRDIR=$(pwd)
-MODIR="/sdcard/Magisk-Module/"
 
 ziping()
 {
@@ -9,13 +8,9 @@ cd $i
 shopt -s extglob
 filename=$(echo $i | sed 's:/*$::')
 zip -qr $filename *
-
-if [ ! -d $MODIR ]; then
-    mkdir $MODIR
-fi
 mv $filename.zip $CRDIR
 cd $CRDIR
-wget https://github.com/ArieSR91/Magisk-Module/$filename.zip
+wget https://raw.githubusercontent.com/ArieSR91/Magisk-Module/refs/heads/main/$filename.zip
 rm *.zip
 }
 
