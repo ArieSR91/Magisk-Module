5e82024a285a703a9adc1a3e96afb1ad8e67dd00
ArieSR91 <syahri.ramadhan2022@gmail.com>
2024-11-20 18:56:43 +0800

    from debian

--- build.sh
+++ build.sh
@@ -8,10 +8,12 @@ ziping()
     cd $i
     shopt -s extglob
     filename=$(echo $i | sed 's:/*$::')
+    echo "Building $filename"
     zip -qr $filename *
     if [ ! -d $MODIR ]; then
         mkdir $MODIR
     fi
+    echo "Moving $filename to $MODIR"
     mv $filename.zip $MODIR
     cd $CRDIR
 }
